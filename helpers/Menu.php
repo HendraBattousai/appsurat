4ad965cd8e2e4b77381cab787038bcab9419e353
Hendra Battousai <71735045+HendraBattousai@users.noreply.github.com>
2022-07-14 10:34:28 +0700

    Add files via upload

---
+++ helpers/Menu.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Menu Items
+ * All Project Menu
+ * @category  Menu List
+ */
+
+class Menu{
+	
+	
+			public static $navbarsideleft = array(
+		array(
+			'path' => 'home', 
+			'label' => 'Home', 
+			'icon' => '<i class="fa fa-home fa-2x"></i>'
+		),
+		
+		array(
+			'path' => 'surat_keluar', 
+			'label' => 'Surat Keluar', 
+			'icon' => '<i class="fa fa-arrow-circle-right fa-2x"></i>'
+		),
+		
+		array(
+			'path' => 'surat_masuk', 
+			'label' => 'Surat Masuk', 
+			'icon' => '<i class="fa fa-arrow-circle-left fa-2x"></i>'
+		),
+		
+		array(
+			'path' => 'pengguna', 
+			'label' => 'Pengguna', 
+			'icon' => '<i class="fa fa-users "></i>'
+		)
+	);
+		
+	
+	
+}
